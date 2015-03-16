61d2b3760ebca66bb18e26307bc88b9f8ca6ce0f
ZYP-linux <zyp@localhost.localdomain>
2015-03-16 05:36:54 -0700

    1srt Version

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: yuepeng
+	> Mail:356364170@qq.com 
+	> Created Time: Mon 16 Mar 2015 05:26:07 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("hello\n");
+	system("pause");
+	return 0;
+}
